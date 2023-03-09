7534bff64158d6a997c24dfbeb889a035434a53a
Bahaa Youssof <b.zaki@egyptsmartcards.com>
2023-03-09 13:20:12 +0200

    add sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import LOGO from "../../assets/images/logo.png";
 import { Autocomplete, Box, Button, TextField } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 import "./sidebar.scss";
-const sidebar = () => {
+const sidebar = props => {
 	const top100Films = [
 		{ label: "The Shawshank Redemption", year: 1994 },
 		{ label: "The Godfather", year: 1972 },
@@ -16,67 +16,69 @@ const sidebar = () => {
 		{ label: "Schindler's List", year: 1993 },
 		{ label: "Pulp Fiction", year: 1994 },
 	];
+
 	return (
 		<Box className="sidebar">
-			<div className="top">
+			{/* <Box className="top">
 				<img className="logo" src={LOGO} alt="Damen" />
-			</div>
+			</Box> */}
 
-			<Box
-				component="form"
-				className="filters"
-				sx={{
-					"& .MuiTextField-root": { m: 1, width: "25ch" },
-					margin: "10px",
-					backgroundColor: "#eee",
-					borderRadius: "5px",
-				}}
-				noValidate
-				autoComplete="off"
-				onSubmit={e => e.preventDefault()}
-			>
-				<TextField
-					name="op_number"
-					id="outlined-required"
-					label="Operation Number"
-					size="small"
-				/>
-				<TextField name="extra_info" id="outlined-required" label="Extra Info" size="small" />
-				<Autocomplete
-					disablePortal
-					id="combo-box-demo"
-					options={top100Films}
-					renderInput={params => <TextField {...params} label="Select Provider" />}
-					sx={{ width: "25ch" }}
-					size="small"
-				/>
-				<Autocomplete
-					disablePortal
-					id="combo-box-demo"
-					options={top100Films}
-					renderInput={params => <TextField {...params} label="Select Service" />}
-					sx={{ width: "25ch" }}
-					size="small"
-				/>
-				<Autocomplete
-					disablePortal
-					id="combo-box-demo"
-					options={top100Films}
-					renderInput={params => <TextField {...params} label="Select Government" />}
-					sx={{ width: "25ch" }}
-					size="small"
-				/>
+			<Box className="filters_container">
+				<Box
+					component="form"
+					className="filters"
+					noValidate
+					autoComplete="off"
+					onSubmit={e => {
+						e.preventDefault();
+					}}
+				>
+					<TextField name="op_number" id="outlined-required" label="Operation Number" />
+					<TextField name="extra_info" id="outlined-required" label="Extra Info" />
+					<Autocomplete
+						disablePortal
+						id="provider_name"
+						options={top100Films}
+						renderInput={params => <TextField {...params} label="Select Provider" />}
+					/>
+					<Autocomplete
+						disablePortal
+						id="service_name"
+						options={top100Films}
+						renderInput={params => <TextField {...params} label="Select Service" />}
+					/>
+					<Autocomplete
+						disablePortal
+						id="gov_code"
+						options={top100Films}
+						renderInput={params => <TextField {...params} label="Select Government" />}
+					/>
 
-				<Box>
-					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<DatePicker label="From Date" onChange={date => console.log(date)} />
-						<DatePicker label="To Date" onChange={date => console.log(date)} />
-					</LocalizationProvider>
+					<Box>
+						<LocalizationProvider dateAdapter={AdapterDayjs}>
+							<DatePicker
+								label="From Date"
+								minDate={dayjs().subtract(1, "month")}
+								onChange={date => console.log(dayjs(date))}
+							/>
+							<DatePicker label="To Date" onChange={date => console.log(date)} />
+						</LocalizationProvider>
+					</Box>
+
+					<Box className="actions">
+						<Button color="error" onClick={() => console.log("reset!")}>
+							reset
+						</Button>
+						<Button onClick={() => console.log("search!")}>search</Button>
+					</Box>
 				</Box>
-
+			</Box>
+			<Box className="bottom">
 				<Box className="actions">
-					<Button>search</Button>
-					<Button color="error">reset</Button>
+					<Button onClick={() => console.log("Change Password!")}>Change Password</Button>
+					<Button color="error" onClick={() => console.log("logout!")}>
+						Logout
+					</Button>
 				</Box>
 			</Box>
 		</Box>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import Theme from "./styles/theme";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider theme={Theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
 );
-
